37232672875ef7554f5fa2e926691fd9494a777d
fidelis <fidelis.marcelo@gmail.com>
2020-10-13 08:53:17 -0300

    My second commit

--- src/pages/OrphanegesMap.tsx
+++ src/pages/OrphanegesMap.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanagesMap.css';
 
 function OrphanageMap() {
     return (
-        <h1>Hello OrphanageMap</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Goiânia</strong>
+                    <span>Goiás</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     );
 }
 
